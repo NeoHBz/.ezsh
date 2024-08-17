de4fdbf5795eb001c74d0403b94bc0befb381f51
NeoHBz <neohbz@gmail.com>
2024-08-17 18:31:19 +0530

    added: os detection for load function

--- load.sh
+++ load.sh
@@ -3,7 +3,19 @@
 # Directory containing zsh function files
 ZSH_FUNCTION_DIR=~/.zsh/functions
 
+# Detect the operating system
+OS_TYPE=$(uname)
+
 # Source all .sh files in the directory and its subdirectories
-for file in $(find $ZSH_FUNCTION_DIR -type f -name '*.sh' -o -name '*.zsh'); do
+for file in $(find $ZSH_FUNCTION_DIR -type f \( -name '*.sh' -o -name '*.zsh' \)); do
+  # Check if file is in a platform-specific folder
+  if [[ "$file" == *"/mac/" && "$OS_TYPE" != "Darwin" ]]; then
+    # Skip if file is in mac-specific folder but not on macOS
+    continue
+  elif [[ "$file" == *"/windows/" && "$OS_TYPE" != *"CYGWIN"* && "$OS_TYPE" != *"MINGW"* && "$OS_TYPE" != "MSYS"* ]]; then
+    # Skip if file is in windows-specific folder but not on Windows
+    continue
+  fi
+  # Source the file
   source "$file"
 done
