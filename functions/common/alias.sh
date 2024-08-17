129500991b59aa2591eb69fb093049f586c64033
NeoHBz <neohbz@gmail.com>
2024-08-17 18:19:55 +0530

    added: common functions

---
+++ functions/common/alias.sh
@@ -0,0 +1,4 @@
+alias pyvenv="python -m venv .venv && source .venv/bin/activate && echo .venv/ >> .gitignore"
+alias prisma="bunx prisma"
+alias prismaup="bunx prisma generate && bunx prisma db push --accept-data-loss"
+alias expo="bunx expo"
---
+++ functions/common/keybind.sh
@@ -0,0 +1,4 @@
+bindkey '^[[A' up-line-or-history
+bindkey '^[[B' down-line-or-history
+bindkey '^[[C' forward-char
+bindkey '^[[D' backward-char
