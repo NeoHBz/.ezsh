c3581fd9739aa523325fab8e7c898a9096c3f689
NeoHBz <neohbz@gmail.com>
2024-08-17 18:25:03 +0530

    added: README

---
+++ README.sh
@@ -0,0 +1,18 @@
+## .zsh
+Custom zsh functions for ease of life
+
+### Installation
+
+1. Clone the repository
+2. Run the following command:
+```
+chmod +x install.sh
+./install.sh
+source ~/.zshrc
+```
+
+### Usage
+TBD
+
+### Contributing to the repository
+TBD
