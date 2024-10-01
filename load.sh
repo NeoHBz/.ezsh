0cdeba15b63368d56e09c7f78b4a3109a93f0a1e
NeoHBz <neohbz@gmail.com>
2024-10-01 17:28:31 +0530

    added: load.sh prefixing to nested folder

--- load.sh
+++ load.sh
@@ -2,25 +2,50 @@
 
 # Directory containing zsh function files
 ZSH_FUNCTION_DIR=~/.zsh/functions
+# ZSH_FUNCTION_DIR=~/.zsh/sandbox
 
-# Detect the operating system
 OS_TYPE=$(uname)
 
-# Source the .env file if it exists
 if [ -f ~/.zsh/.env ]; then
   source ~/.zsh/.env
 fi
 
-# Source all .sh and .zsh files in the directory and its subdirectories
 find $ZSH_FUNCTION_DIR -type f \( -name '*.sh' -o -name '*.zsh' \) | while read file; do
-  # Check if file is in a platform-specific folder
   if [[ "$file" == */mac/* && "$OS_TYPE" != "Darwin" ]]; then
-    # Skip if file is in mac-specific folder but not on macOS
     continue
   elif [[ "$file" == */windows/* && "$OS_TYPE" != CYGWIN* && "$OS_TYPE" != MINGW* && "$OS_TYPE" != MSYS* ]]; then
-    # Skip if file is in windows-specific folder but not on Windows
     continue
   fi
-  # Source the file
-  source "$file"
+
+  # Get the relative path from the function directory
+  relative_path="${file#$ZSH_FUNCTION_DIR/}"
+
+  if [[ "$relative_path" == */* ]]; then
+    prefix_path="${relative_path#*/}"
+    prefix_path="${prefix_path%/*}"
+    prefix=$(echo "$prefix_path" | tr '/' '_')
+
+    source "$file"
+
+    function_name=$(basename "$file" .sh)
+
+    # Check if the file contains a function
+    if grep -q "^$function_name() {" "$file"; then
+      # Read the entire file content
+      function_body=$(<"$file")
+
+      # Extract the content inside the function (removing the function definition)
+      function_body=$(echo "$function_body" | sed -n "/^$function_name() {/,/^}/p" | sed '1d;$d')
+
+      # Construct the prefixed function to directly execute the body
+      eval "${prefix}${function_name}() { $function_body }"
+
+      unset -f "${function_name}"
+    else
+      # For non-function files (aliases, variables), source them directly
+      source "$file"
+    fi
+  else
+    source "$file"
+  fi
 done
