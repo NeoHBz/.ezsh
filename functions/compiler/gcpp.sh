d511ee7eaedee34fc71b67fa1709cf1a9fe6b651
NeoHBz <neohbz@gmail.com>
2025-01-02 20:35:42 +0530

    changed: gcc opener for macos

--- functions/compiler/gcpp.sh
+++ functions/compiler/gcpp.sh
@@ -1,8 +1,12 @@
 gcpp() {
     # check if passed argument already has .cpp extension, if yes, remove it
     local filename=$1
+    local launch_program="g++"
+    if [[ "$OS_TYPE" == "Darwin" ]]; then
+        launch_program="/opt/homebrew/bin/g++-14"
+    fi
     if [[ $filename == *.cpp ]]; then
         filename=${filename%.cpp}
     fi
-    g++ -std=c++11 -o $filename $filename.cpp && ./$filename && rm $filename
+    $launch_program -std=c++11 -o $filename $filename.cpp && ./$filename && rm $filename
 }
