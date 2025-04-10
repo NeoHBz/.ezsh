ed04bb8299c9669becabb9e2754f6dbad123ed3d
NeoHBz <neohbz@gmail.com>
2025-04-11 00:08:36 +0530

    added: docker/remote remove command

--- functions/docker/remote.sh
+++ functions/docker/remote.sh
@@ -1,3 +1,7 @@
 export DOCKER_TLS_VERIFY=$DOCKER_TLS_VERIFY
 export DOCKER_CERT_PATH=$DOCKER_CERT_PATH
 export DOCKER_HOST=$DOCKER_HOST
+
+removeremote() {
+    unset $(env | grep ^DOCKER | cut -d= -f1 | tr '\n' ' ' | sed 's/ $//')
+}
